4475657a0d0025f8e65e5e64c51663d23c20671e
Sam Games <kwisssamir00119922@gmail.com>
2025-07-25 16:13:50 +0100

    Update script.js
    Signed-off-by: Sam Games <kwisssamir00119922@gmail.com>

--- script.js
+++ script.js
@@ -201,29 +201,4 @@ document.querySelectorAll(".category-box").forEach(box => {
 
 
 
-  // عرض البنر إذا لم تكن هناك موافقة أو رفض
-  window.addEventListener('DOMContentLoaded', () => {
-    const banner = document.getElementById('cookie-banner');
-    if (!localStorage.getItem('cookie-consent')) {
-      banner.style.display = 'block';
-    }
 
-    document.getElementById('accept-cookies').onclick = () => {
-      localStorage.setItem('cookie-consent', 'accepted');
-      banner.style.display = 'none';
-
-      // Google Analytics script
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'G-07SBG11V92', 'auto');
-      ga('send', 'pageview');
-    };
-
-    document.getElementById('reject-cookies').onclick = () => {
-      localStorage.setItem('cookie-consent', 'rejected');
-      banner.style.display = 'none';
-    };
-  });
